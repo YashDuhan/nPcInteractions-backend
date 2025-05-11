55996bb3b2d6a6ae306363655173d050dc92a6d1
Yash Duhan <70028651+YashDuhan@users.noreply.github.com>
2025-05-11 13:45:35 +0530

    updated routes

--- app/api/routes.py
+++ app/api/routes.py
@@ -3,8 +3,8 @@ from .endpoints import chat_ankit, chat_aman, chat_aakash
 
 app_router = APIRouter()
 
-app_router.get("/chat-ankit")(chat_ankit)
+app_router.post("/chat-ankit")(chat_ankit)
 
-app_router.get("/chat-aman")(chat_aman)
+app_router.post("/chat-aman")(chat_aman)
 
-app_router.get("/chat-aakash")(chat_aakash)
+app_router.post("/chat-aakash")(chat_aakash)
